99e8fd99ed5b8b9771ae9ab5f3fdb8eab70dc8fa
AkKeRise <akkerise@gmail.com>
2019-01-12 17:03:24 +0700

    edited module service provider

--- app/Modules/ModuleServiceProvider.php
+++ app/Modules/ModuleServiceProvider.php
@@ -1,29 +1,38 @@
 <?php
+
 namespace App\Modules;
 
-class ModuleServiceProvider extends  \Illuminate\Support\ServiceProvider{
-    public function boot(){
+class ModuleServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot()
+    {
         $modules = array_map('basename', \File::directories(__DIR__));
         foreach ($modules as $module) {
-            $this->bootRoutes($module);
+            $this->bootApiRoutes($module);
+            $this->bootWebRoutes($module);
             $this->bootViews($module);
         }
     }
 
-    public function register(){}
-
-    public function bootRoutes($module){
-        if(file_exists(__DIR__.'/'.$module.'/Routes/web.php')){
-            $this->loadRoutesFrom(__DIR__.'/'.$module.'/Routes/web.php');
-        }
-        if(file_exists(__DIR__.'/'.$module.'/Routes/api.php')){
-            $this->loadRoutesFrom(__DIR__.'/'.$module.'/Routes/api.php');
-        }
+    public function register()
+    {
     }
 
-    public function bootViews($module){
-        if(is_dir(__DIR__.'/'.$module.'/Resources/Views')) {
-            $this->loadViewsFrom(__DIR__.'/'.$module.'/Resources/Views', $module);
-        }
+    public function bootApiRoutes($module)
+    {
+        if (file_exists(__DIR__ . "/{$module}/Routes/api.php"))
+            $this->loadRoutesFrom(__DIR__ . "/{$module}/Routes/api.php");
+    }
+
+    public function bootWebRoutes($module)
+    {
+        if (file_exists(__DIR__ . "/{$module}/Routes/web.php"))
+            $this->loadRoutesFrom(__DIR__ . "/{$module}/Routes/web.php");
+    }
+
+    public function bootViews($module)
+    {
+        if (is_dir(__DIR__ . '/' . $module . '/Resources/Views'))
+            $this->loadViewsFrom(__DIR__ . "/{$module}/Resources/Views", $module);
     }
 }
